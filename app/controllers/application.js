61973fe83cc83e8c22f0273e9f2c7fdfcdf0424a
Arlston Fernandes <archferns@gmail.com>
2018-03-24 16:01:58 +0530

    1.5 basic data types and enumerables

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,36 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import Object, { computed } from '@ember/object';
+
+const LineItem = Object.extend({
+  init(){
+    this._super(...arguments);
+    this.set('date', new Date());
+  },
+  description: 'Some text',
+  amount: 0,
+  isExpense: true
+});
 
 export default Controller.extend({
-  firstName: 'Johnny',
-  lastName: 'Cash',
-  fullName: computed('firstName', 'lastName', {
-    get(){
-      return `${this.get('firstName')} ${this.get('lastName')}`;
-    },
-    set(key, value){
-      let [firstName, lastName] = value.split(/\s+/);
-      this.set('firstName', firstName);
-      this.set('lastName', lastName);
-      return value;
-    }
+  init(){
+    this._super(...arguments);
+    this.set('lineItems', [
+      LineItem.create({ description: 'Rent Payment', date: new Date(2017, 10, 3), amount: 900}),
+      LineItem.create({ description: 'Dinner', date: new Date(2017, 10, 4), amount: 100}),
+      LineItem.create({ description: 'Fuel', date: new Date(2017, 10, 5), amount: 150})
+      ]
+    )
+  },
+  lineItemsOutput: computed('lineItems.[]', function(){
+    let lineItems = this.get('lineItems'),
+    output = '';
+
+    lineItems.forEach((item) => {
+      output += `Description: ${item.get('description')}-`;
+      output += `Date: ${item.get('date')}-`;
+      output += `Amount: ${item.get('amount')}-`;
+      output += `Expense: ${item.get('isExpense')}`;
+    });
+    return output;
   })
 });
