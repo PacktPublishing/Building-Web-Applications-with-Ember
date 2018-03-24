df6a6b118afcd71d28975a5aa23f4cb0af9236fe
Arlston Fernandes <archferns@gmail.com>
2018-03-24 23:34:05 +0530

    4.2 Authentication with torii

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,28 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
   model(){
     return this.get('store').findAll('line-item');
+  },
+  session: service(),
+  beforeModel() {
+      return this.get('session').fetch().catch(function() {});
+  },
+  actions: {
+    signIn() {
+      let email = this.controllerFor('application').get('userEmail'),
+      password = this.controllerFor('application').get('userPassword');
+      this.get('session').open('firebase', {
+        provider: 'password',
+        email: email,
+        password: password
+      }).then(() => {
+        this.transitionTo('index');
+      });
+    },
+    signOut() {
+      this.get('session').close();
+    }
   }
 });
